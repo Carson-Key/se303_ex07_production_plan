1f6254a4218ac0c0fbf9c9c71f40e0c8cb5770f5
Carson-Key <keyc@oregonstate.edu>
2021-05-16 16:09:46 -0700

    Fix setup to better reflect Producer

--- test/producer_test.rb
+++ test/producer_test.rb
@@ -2,11 +2,14 @@ gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/producer'
+require_relative '../lib/province'
+require_relative '../lib/data'
 
 class ProducerTest < Minitest::Test
 
     def setup
-        @asia = Province.new(sample_province_data)
+        @asia_province = Province.new(sample_province_data)
+        @asia = Producer.new(@asia_province, sample_province_data[0])
     end
 
 end
